db9319d37045dc853517d1e95b91cdde313f14cd
Laura Totten <laura.dwa15@gmail.com>
2013-09-15 11:47:56 -0400

    added photo and changed css

--- index.php
+++ index.php
@@ -5,13 +5,18 @@
 <link href="p1.css" media="all" rel="stylesheet" type="text/css" />
 </head>
 
-<body>
-	<h1>Hello!</h1>
-	<p>My name is Laura Totten and I'm a big fan of TextWrangler.</p>
+<body>	
+	<div style="float: right;">
+			<img src="p1headshot.jpg" alt="head shot" border="1px solid black"/>
+	</div>
+	<div>
+		<h1>Hello!</h1>
+		<p>My name is Laura Totten and I'm a big fan of TextWrangler.</p>
 	
-	<p>I'm starting a new job on Monday as Web Developer for Harvard's 
-	Digital Strategy team, and I'm over the moon excited to be taking
-	this class simultaneously. I feel that a double reinforcement of
-	these concepts will prove invaluable.</p>
+		<p>I'm starting a new job on Monday as Web Developer for Harvard's 
+		Digital Strategy team, and I'm over the moon excited to be taking
+		this class simultaneously. I feel that a double reinforcement of
+		these concepts will prove invaluable.</p>
+	</div>
 </body>
 </html>
