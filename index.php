644ad5382ffc303b84959b32f95cb12ceb19afd9
Laura Totten <laura.dwa15@gmail.com>
2013-09-15 11:04:37 -0400

    added basic content to index page

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
+<!DOCTYPE html>
 <html>
+<head>
+<title>Laura Totten</title>
+<link href="p1.css" media="all" rel="stylesheet" type="text/css" />
+</head>
+
 <body>
-	<h1>Welcome to P1</h1>
+	<h1>Hello!</h1>
+	<p>My name is Laura Totten and I'm a big fan of TextWrangler.</p>
+	
+	<p>I'm starting a new job on Monday as Web Developer for Harvard's 
+	Digital Strategy team, and I'm over the moon excited to be taking
+	this class simultaneously. I feel that a double reinforcement of
+	these concepts will prove invaluable.</p>
 </body>
 </html>
